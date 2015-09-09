dfb6e4288f48370a556f604784d9be6f475f0197
Nicolas Leroy <nicolas.leroy@gmail.com>
2015-09-09 14:13:43 +0200

    Correct and add providers to index.php

--- web/index.php
+++ web/index.php
@@ -1,10 +1,13 @@
 <?php
 use CultuurNet\Auth\ConsumerCredentials;
-use CultuurNet\SymfonySecurityOAuth\OAuthAuthenticationProvider;
-use CultuurNet\SymfonySecurityOAuth\OAuthListener;
+use CultuurNet\SymfonySecurityOAuth\Security\OAuthAuthenticationProvider;
+use CultuurNet\SymfonySecurityOAuth\Security\OAuthListener;
+use CultuurNet\SymfonySecurityOAuth\Service\OAuthServerService;
+use CultuurNet\SymfonySecurityOAuthUitid\ConsumerProvider;
+use CultuurNet\SymfonySecurityOAuthUitid\NonceProvider;
+use CultuurNet\SymfonySecurityOAuthUitid\TokenProvider;
 use CultuurNet\UitidCredentials\UitidCredentialsFetcher;
 use DerAlex\Silex\YamlConfigServiceProvider;
-use Symfony\Component\HttpFoundation\Request;
 
 require_once __DIR__.'/../vendor/autoload.php';
 
@@ -12,21 +15,42 @@ $app = new Silex\Application();
 
 $app->register(new YamlConfigServiceProvider(__DIR__ . '/../config.yml'));
 
-
-$app['oauth.fetcher'] = $app->share(function ($app) {
-  $baseUrl = $app['config']['oauth.base_url'];
-  $consumerkey = $app['config']['oauth.consumer.key'];
-  $consumersecret = $app['config']['oauth.consumer.secret'];
+$app['oauth.fetcher'] = $app->share(function () use ($app) {
+  $baseUrl = $app['config']['oauth']['base_url'];
+  $consumerkey = $app['config']['oauth']['consumer']['key'];
+  $consumersecret = $app['config']['oauth']['consumer']['secret'];
 
   $consumerCredentials = new ConsumerCredentials($consumerkey, $consumersecret);
 
   return new UitidCredentialsFetcher($baseUrl, $consumerCredentials);
 });
 
+$app['oauth.model.provider.consumer_provider'] = $app->share(function ($app) {
+    return new ConsumerProvider($app['oauth.fetcher']);
+});
+
+$app['oauth.model.provider.token_provider'] = $app->share(function ($app) {
+    return new TokenProvider($app['oauth.fetcher']);
+});
+
+$app['oauth.model.provider.nonce_provider'] = $app->share(function () {
+    return new NonceProvider();
+});
+
+$app['oauth.service.oauth_server_service'] = $app->share(function () use ($app) {
+    $consumerProvider = $app['oauth.model.provider.consumer_provider'];
+    $tokenProvider = $app['oauth.model.provider.token_provider'];
+    $nonceProvider = $app['oauth.model.provider.nonce_provider'];
+    return new OAuthServerService($consumerProvider, $tokenProvider, $nonceProvider);
+});
+
 $app['security.authentication_listener.factory.oauth'] = $app->protect(function ($name, $options) use ($app) {
   // define the authentication provider object
   $app['security.authentication_provider.'.$name.'.oauth'] = $app->share(function () use ($app) {
-    return new OAuthAuthenticationProvider($app['security.user_provider.default'], __DIR__.'/security_cache', $app['oauth.fetcher']);
+    return new OAuthAuthenticationProvider(
+        $app['security.user_provider.default'],
+        $app['oauth.service.oauth_server_service'] //__DIR__.'/security_cache',
+    );
   });
 
   // define the authentication listener object
@@ -56,9 +80,11 @@ $app->register(new Silex\Provider\SecurityServiceProvider(), array(
 ));
 
 $app['oauth.request_listener'] = $app->share(function() {
-  return new \CultuurNet\SymfonySecurityOAuth\OAuthRequestListener();
+  return new \CultuurNet\SymfonySecurityOAuth\EventListener\OAuthRequestListener();
 });
 
 $app['dispatcher']->addListener('kernel.request', array($app['oauth.request_listener'], 'onEarlyKernelRequest'), 255);
+
+$app->get('test', function () { return new \Symfony\Component\HttpFoundation\Response();} );
 
 $app->run();
